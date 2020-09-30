ae98ac7faa33dcec6d965b5629869f9f45561870
Salt <luffy.w.pirate@gmail.com>
2020-10-01 00:37:24 +0800

    Children routes

--- src/router.js
+++ src/router.js
@@ -1,11 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+// import ResourceRoutes from '@/pages/ResourceRoutes';
 import ResourceHome from '@/pages/ResourceHome';
 import ResourceNew from '@/pages/ResourceNew';
 
+// const routes = [
+//   { path: '/', redirect: { name: 'resourceHome' } },
+//   {
+//     path: '/resources',
+//     name: 'resourcesRoutes',
+//     component: ResourceRoutes,
+//     children: [
+//       { path: '', name: 'resourceHome', component: ResourceHome },
+//       { path: 'new', name: 'resourceNew', component: ResourceNew },
+//     ],
+//   },
+// ];
+
 const routes = [
-  { path: '/', component: ResourceHome },
-  { path: '/new', component: ResourceNew },
+  { path: '/', redirect: { name: 'resourceHome' } },
+  { path: '/resources', name: 'resourceHome', component: ResourceHome },
+  { path: '/resources/new', name: 'resourceNew', component: ResourceNew },
 ];
 
 const router = createRouter({
